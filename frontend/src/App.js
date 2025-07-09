a96294a91b43369038a5cde5d8d8a0294a54f609
Vincent <158262786+vdolera@users.noreply.github.com>
2025-07-09 15:00:30 +0800

    App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
-import Login from "./Login";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
 
-function App() {
-  return <Login />;
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App;
