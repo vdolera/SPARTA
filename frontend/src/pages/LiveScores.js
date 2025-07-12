c956a232c27ecde63151802f26293dc146ff7074
wanyuuu <158263091+YuanMLP@users.noreply.github.com>
2025-07-12 23:23:13 +0800

    Added MainLayout Component

--- frontend/src/pages/LiveScores.js
+++ frontend/src/pages/LiveScores.js
@@ -1,16 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SideMenu from '../components/SideMenu';
 import Header from "../components/Header";
+import "../styles/LiveScores.css";
 
 const LiveScores = () => {
   return (
 
     <div style={{ display: 'flex' }}>
+
       <SideMenu />
       <Header />
-      <div style={{ marginLeft: '220px', padding: '20px' }}>
-        {/* Add Routes or content here */}
+      <div className="live-scores-content">
+      {
+        <h1>Live Scores</h1>       
+      }
       </div>
+
+
     </div>
 
   )
