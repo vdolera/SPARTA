84315b24a57535ac214980b876154c7a1fbada01
wanyuuu <158263091+YuanMLP@users.noreply.github.com>
2025-08-06 13:48:25 +0800

    Created Feedback Template

--- frontend/src/pages/admin/ADMIN_Feedback.js
+++ frontend/src/pages/admin/ADMIN_Feedback.js
@@ -1,10 +1,27 @@
 import MainLayout from "../../components/MainLayout";
+import React from "react";
+import '../../styles/ADMIN_Feedback.css';
 
 const Feedback = () => {
   return (
 
     <MainLayout>
       <h1>Feedback</h1>
+
+    <div className="feedback-maindiv">
+      
+      <div className="feedback-container">
+
+        <div className="feedback-contents">
+          {/* Feedback items will be displayed here */}
+          <h3>Feedback title</h3>
+          <p>User1: This is a feedback comment.</p>
+        </div>
+        
+      </div>
+
+    </div>
+
     </MainLayout>
 
   )
