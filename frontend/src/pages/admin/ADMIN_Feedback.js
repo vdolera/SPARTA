7766f53b960d2d7442e1a60107a88ad6e19533e6
wanyuuu <158263091+YuanMLP@users.noreply.github.com>
2025-08-11 01:09:24 +0800

    Updated ADMIN_Feedback.css and .js

--- frontend/src/pages/admin/ADMIN_Feedback.js
+++ frontend/src/pages/admin/ADMIN_Feedback.js
@@ -6,7 +6,6 @@ const Feedback = () => {
   return (
 
     <MainLayout>
-      <h1>Feedback</h1>
 
     <div className="feedback-maindiv">
       
@@ -14,8 +13,13 @@ const Feedback = () => {
 
         <div className="feedback-contents">
           {/* Feedback items will be displayed here */}
-          <h3>Feedback title</h3>
+          <div className="feedback-header-row">
+            <h3>Feedback Title</h3>
+            <h5 style={{ fontStyle: "italic" }}>Feedback Date</h5>
+          </div>
+
           <p>User1: This is a feedback comment.</p>
+
         </div>
         
       </div>
